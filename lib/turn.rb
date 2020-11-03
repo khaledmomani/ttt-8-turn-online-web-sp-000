9dd096a3214f0c7d2b3db7bfa42b7b6ac6169b6c
Khaled S Momani <khaledmomani4@gmail.com>
2020-11-03 18:42:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -47,13 +47,13 @@ def move(board, index, character = "X")
   return board
 end
 
-#Asks for number loop
+#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
 def turn (board)
-  puts "Please enteter 1-9:"
+  puts "Please enter 1-9:"
   num = gets.chomp
-  index = input_to_index (num)
-  if valid_move? (board, index) == true
-    move(board,index)
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
     display_board(board)
   else
     turn(board)
