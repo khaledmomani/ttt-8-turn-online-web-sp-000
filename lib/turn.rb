515e8e5904dfbe37f51befbb5a7efa49621a8a38
Khaled S Momani <khaledmomani4@gmail.com>
2020-11-03 18:34:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,20 +16,34 @@ def input_to_index(user_input)
 end
 
 
+#checks the number to see if 1) the position is already taken and 2)it is an actual number
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
 
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 
-
-
-def position_taken?(board,index)
-  if board[position] == " "
-    false
-    elsif board[position] == ""
-    false
-    elsif  board[position] == nil
-    false
-    else   board[position] == "X" || "O"
-    true
-
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
   end
 end
+
+
+
+def move(board, index, character = "X")
+  board {index = character
+  return board
 end
