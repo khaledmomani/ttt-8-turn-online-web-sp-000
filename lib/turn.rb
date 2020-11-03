3bec0172259ac04caea239fd590c88d0d3154362
Khaled S Momani <khaledmomani4@gmail.com>
2020-11-03 18:30:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 def turn
 
-  def display_board(board = [ ])
+  def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -9,8 +9,10 @@ def turn
 end
 
 
-def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
+def input_to_index(user_input)
   user_input.to_i - 1
+  index -= 1
+  return inxdex
 end
 
 
