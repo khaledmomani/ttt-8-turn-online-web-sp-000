6309ad7cc8919dd184fda0800904b995530f3d52
Khaled S Momani <khaledmomani4@gmail.com>
2020-11-03 18:25:51 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,43 @@
+def turn
+
+  def display_board(board = [ ])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
+  user_input.to_i - 1
+end
+
+
+def move(board, index, current_player = "X")
+    board [index] = current_player
+  end
+  
+  def valid_move?(board,index)
+  if  inxdex.to_i.between?(1,9) && position_taken?
+    true
+  else
+     false
+  end
+end
+
+
+
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+
+  end
+end
+end
